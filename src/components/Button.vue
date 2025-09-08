90fd00d4c61b7d1b1f28cf50afb9bbd72c6c94d0
Andrey Shurygin <andrey.sh@solargroup.team>
2025-09-08 15:08:04 +0300

    2-button

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  min-width: 335px;
+  padding: 16px 10px;
+  border-radius: 100px;
+  border: none;
+  background: none;
+  font-family: inherit;
+  font-size: 24px;
+  line-height: 1.5;
+  color: #ffffff;
+  background-color: var(--color-bg-button);
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: var(--color-bg-button-hover);
+}
+</style>
